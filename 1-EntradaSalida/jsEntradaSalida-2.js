2fd7aa0672e13a12efff795004258ca9f223dd7a
AldrianAzocar <40859199+AldrianAzocar@users.noreply.github.com>
2018-07-05 11:24:53 -0300

    primer commit
    estos son los 4 ejercicios de la primera clase.

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,19 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
-}
+//var person = prompt("Please enter your name", "Harry Potter");
 
+/*	
+esto 
+es
+un
+comentario
+de
+bloque
+*/
+	var nombre;
+	//nombre = "aldrian azocar";
+	//alert(nombre);
+	nombre= prompt("ingrese su nombre");
+	alert("su nombre es: "+ nombre);
+}
