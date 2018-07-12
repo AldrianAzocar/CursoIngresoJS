46ce210914ea1825a6305a31df82f93bb3ef5523
AldrianAzocar <40859199+AldrianAzocar@users.noreply.github.com>
2018-07-12 11:51:13 -0300

    Instruccion F

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,22 @@
 function mostrar()
 {
 //tomo la edad  
+	var edad;
+	edad=document.getElementById('edad').value;
+	if(edad<18)
+	{	
+		if(edad>12)
 
-
+		{
+			
+		}
+		else
+		{
+			alert("usted No es adolescente");
+		}
+	}
+		else
+		{
+			alert("usted No es adolescente");
+		}	
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,22 @@
 function mostrar()
 {
 //tomo la edad  
+	var edad
+	edad=document.getElementById('edad').value;
+	if(edad>=18)
 
-
+	{
+		alert("usted es mayor de edad");
+	}	
+	if(edad>=13)
+	if(edad<=17)
+	{
+		alert("usted es adolescente");
+	}	
+	if(edad<13)
+	{
+		alert("usted es un niño");
+	}		
 
 
 }//FIN DE LA FUNCIÓN
