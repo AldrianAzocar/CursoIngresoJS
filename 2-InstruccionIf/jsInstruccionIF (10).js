4627f69b0f7f4163e82cafe3b05c76e7acde0853
AldrianAzocar <40859199+AldrianAzocar@users.noreply.github.com>
2018-07-12 12:23:11 -0300

    ejercicios 9 y 10

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,19 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var numero
+	numero=Math.floor(Math.random() * 10) + 1;
+	if(numero>= 9 )
+	{
+		alert("EXCELENTE" + numero);
+	}
+	if(numero > 4 && numero <9 )
+		{
+			alert("APROBO" + numero);
+		}
+	if(numero < 4 )
+		{
+			alert("Vamos la proxima se puede" + numero);
+		}
 
 }//FIN DE LA FUNCIÓN
